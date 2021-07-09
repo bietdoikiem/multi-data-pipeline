a7d6f88389d2b499bcd4910800efab90f8e7e338
CallMeDKay <s3812649@rmit.edu.vn>
2021-07-09 22:08:22 +0700

    :sparkles: customize access option for Cassandra's CQLSH

--- container-bash.sh
+++ container-bash.sh
@@ -1,7 +1,19 @@
-#!/bin/bash
+#!/bin/bash -i
 
-echo "Which container do you want to access?"
-read -r CONTAINER;
+echo -n "Which container do you want to access? >"
+read -r CONTAINER
 
-echo "Accessing $CONTAINER..."
-docker exec -it "$CONTAINER" bash
+echo "Accessing $CONTAINER container..."
+if [ "$CONTAINER" == "cassandra" ]
+then
+  echo -n "Do you want to access CQLSH? (y/n) > "
+  read -r CQLSH_OPTION
+  if [ "$CQLSH_OPTION" == "y" ]
+  then
+    docker exec -it "$CONTAINER" "./cql.sh"
+  else
+    docker exec -it "$CONTAINER" bash
+  fi
+else 
+  docker exec -it "$CONTAINER" bash
+fi
