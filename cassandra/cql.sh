5ee02d7ff03f1ecc892f773b846d155f932fcf38
CallMeDKay <s3812649@rmit.edu.vn>
2021-07-09 21:09:07 +0700

    :adhesive_bandage: slight fix cql and run scripts to function properly

--- cassandra/cql.sh
+++ cassandra/cql.sh
@@ -3,4 +3,4 @@ echo "Accessing Cassandra QL Manager..."
 echo "What is the version of CQL?"
 read -r CQL_VERSION
 
-docker exec -it cassandra bash "cqlsh --cqlversion=$CQL_VERSION 127.0.0.1"
+cqlsh --cqlversion="$CQL_VERSION" 127.0.0.1
